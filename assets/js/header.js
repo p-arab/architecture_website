f0189c17431c0b4af9af964f9a943dfc9d2fe378
unknown <blackkapish@yahoo.com>
2024-04-07 10:07:05 +0330

    remove iframe, separate independent pages

---
+++ assets/js/header.js
@@ -0,0 +1,15 @@
+function showProjects() {
+    window.location.href = "../src/Projects.html"
+}
+
+function showAboutUs() {
+    window.location.href = "../src/about_us.html"
+}
+
+function showContactUs() {
+    window.location.href = "../src/contact_us.html"
+}
+
+function showProjectDetails() {
+    window.location.href = "../src/project_details.html"
+}
